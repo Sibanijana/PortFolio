13acb94a2a7c4784a17339af9ddb71384c2822f0
sibani03 <115967828+Sibanijana@users.noreply.github.com>
2024-05-24 15:50:55 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+document.getElementById("contactForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+  console.log(this);
+  emailjs.sendForm("service_g6gbc8n", "template_0bsy3vm", this).then(
+    function (result) {
+      alert("Success!! Your query is generated in our system.");
+      console.log(result.text);
+      document.getElementById("contactForm").reset();
+    },
+    function (error) {
+      alert("Error! Something went wrong.");
+      console.log(error.text);
+    }
+  );
+});
